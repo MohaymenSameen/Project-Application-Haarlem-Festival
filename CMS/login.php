9cf87d708be472b9192944213ba060dde660e2fd
Mish97 <mihaela.yordanova8@gmail.com>
2020-01-19 00:09:48 +0100

    nn

--- CMS/login.php
+++ CMS/login.php
@@ -13,37 +13,37 @@ include ('controller/loginController.php');
         <link href="css/style-login.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <nav>
-            <ul class="main">
-                <li><a href="Home.html">Home</a></li>
-                <li><a href="Jazz.html">Jazz</a></li>
-                <li><a href="Dance.html">Dance</a></li>
-                <li><a href="Food.html">Food</a></li>
-                <li><a href="login.html">Volunteer</a></li>
-            </ul>
-            <ul class="secondary">
-                <li><a>EN</a></li>
-                <li><a>|</a></li>
-                <li><a>NL</a></li>
-            </ul>
-            <div id="logo_square">
-                    <div id="logo_div" class=""></div>
+    <div class="navbar">
+        <ul>
+            <li><a href="../../Home/view/home_view.php">Home</a></li>
+            <li><a href="jazz_view.php"><strong>Jazz</strong></a></li>
+            <li><a href="Dance.html">Dance</a></li>
+            <li><a href="Food.html">Food</a></li>
+            <li><a href="../CMS/admin.php">Volunteer</a></li>
+
+           
+           <div class="dropdown">
+            <button class="dropbtn" onclick="myFunction()"><img src="../Jazz/img/shopping_cart.png"><span>Items</span></button>     
+
+            <div id="myDropdown" class="dropdown-content">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
             </div>
-            <div id="logo">
-                    <img id="logo_img" src="img/logo.png"/>
             </div>
-        </nav>
+        </ul>
+      </div>
         <section>     
             <img  id="login" class="center" src="img/loginIcon.png" alt="Login icon">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">>
-                <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">  
+            <form class="forma" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>>  
                     <label> <img clas="iconsLogin"  src="img/unameicon.png" height="42" width="42"/><input type="text" name = "email" placeholder="E-mail..." class="form-control" value="<?php echo $email; ?>"></label> <!--premesti go v stylesheet-->
-                    <span class="help-block"><?php echo $email_err; ?></span>
+                    <span><?php echo $email_err; ?></span>
                 </div>
 
-                <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <div  <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>>
                     <label><img clas="iconsLogin" src="img/passicon.png" height="35" width="31"/><input type="password" placeholder="Password..." name="password" class="form-control"></label>
-                    <span class="help-block"><?php echo $password_err; ?></span>
+                    <span><?php echo $password_err; ?></span>
                 </div>    
                 
                 <input type="submit" id="loginButton" value="LOGIN">
